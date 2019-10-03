29fd03c51c0a7bba9a3b0a309a4f0e59ba77c71d
David <github.site@torick.net>
2019-10-02 22:04:45 -0400

    Fix invalid cross layer dependency

---
+++ src/SmartHomeDotNet.Package/_ThirdParties/Hass/Commands/SetSpeedExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using SmartHomeDotNet.Hass.Entities;
+using SmartHomeDotNet.SmartHome.Commands;
+using SmartHomeDotNet.SmartHome.Devices;
+using SmartHomeDotNet.Utils;
+
+namespace SmartHomeDotNet.Hass.Commands
+{
+	/// <summary>
+	/// Device extensions for the <see cref="SetSpeed"/> command
+	/// </summary>
+	public static class SetSpeedExtensions
+	{
+		/// <summary>
+		/// Sets the speed of one this fan
+		/// </summary>
+		/// <returns>An <see cref="AsyncContextOperation"/>.</returns>
+		public static AsyncContextOperation SetSpeed<T>(this IDevice<T> device, Fan.Speeds speed)
+			where T : ISupport<TurnOff>
+			=> device.Host.Execute(new SetSpeed(speed), device);
+	}
+}
