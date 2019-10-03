de30ce4b2807459a0e1a5731e0d2e383ec383ac9
David <github.site@torick.net>
2019-10-02 22:34:20 -0400

    Add the Xiaomi OccupancySensor

---
+++ src/SmartHomeDotNet.Package/_ThirdParties/Xiaomi/OccupancySensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using SmartHomeDotNet.SmartHome.Devices;
+
+namespace SmartHomeDotNet.Xiaomi.Devices
+{
+	public class OccupancySensor : Device
+	{
+		public bool IsOccupied => bool.Parse(Raw.occupancy);
+
+		public int Illuminance => int.Parse(Raw.illuminance, CultureInfo.InvariantCulture);
+	}
+}
