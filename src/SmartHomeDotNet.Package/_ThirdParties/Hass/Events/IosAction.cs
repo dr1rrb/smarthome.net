8924d75a299ca2eda204764e9201ed445eb75605
David <github.site@torick.net>
2020-01-25 23:02:44 -0500

    Add the HA IosAction event

---
+++ src/SmartHomeDotNet.Package/_ThirdParties/Hass/Events/IosAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace SmartHomeDotNet.Hass.Events
+{
+	public class IosAction
+	{
+		public Guid ActionId { get; set; }
+
+		public string ActionName { get; set; }
+
+		[JsonPropertyName("sourceDevicePermanentID")]
+		public Guid SourceDeviceUuid { get; set; }
+
+		public string SourceDeviceId { get; set; }
+
+		public string SourceDeviceName { get; set; }
+
+		public string TriggerSource { get; set; }
+	}
+}
