c372be1dfce7d3e1dcbad94dc5bef2663d061617
David <github.site@torick.net>
2021-01-10 23:28:10 -0500

    feat(audio): Add support of audio devices for computers

---
+++ SmartHomeDotNet.Computer.Audio/AudioDeviceIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace SmartHomeDotNet.Computer.Audio
+{
+	public readonly struct AudioDeviceIdentifier
+	{
+		public AudioDeviceIdentifier(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get; }
+	}
+}
---
+++ SmartHomeDotNet.Computer.LocalTests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using SmartHomeDotNet.Computer.Audio;
+using SmartHomeDotNet.SmartHome.Commands;
+using SmartHomeDotNet.SmartHome.Devices_2;
+
+namespace SmartHomeDotNet.Computer.LocalTests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the dev console!");
+
+			var devices = AudioDeviceHost.GetDeviceIds();
+			
+			var host = new AudioDeviceHost();
+			var registry = new DeviceRegistry(Scheduler.Immediate);
+
+			registry.RegisterHost(host);
+
+			var dev = registry.Bind<Microphone>(new AudioDeviceIdentifier(devices.First()));
+			//var dev = registry.Bind<Microphone>(new AudioDeviceIdentifier("Microphone (2- Logitech G933 Gaming Headset)"));
+			//var dev = registry.Bind<Microphone>(new AudioDeviceIdentifier("Casque (PXC 550 Stereo)"));
+
+			dev.TurnOff().ToTask().Wait();
+		}
+	}
+}
